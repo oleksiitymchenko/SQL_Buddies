eae2e4e641e7223bf8d26568dd7ee0ba9c6530da
Oleksii Tymchenko <olexiytimchenko@gmail.com>
2019-02-07 16:45:26 +0200

    MEGACOMMIT

---
+++ main.js
@@ -0,0 +1,21 @@
+document.addEventListener("DOMContentLoaded", function() {
+  const first = document.getElementById("first");
+  const second = document.getElementById("second");
+  const third = document.getElementById("third");
+  const form = document.getElementById("form");
+  setTimeout(function(){ first.classList.add("hide") }, 6000);
+  setTimeout(function(){ second.classList.remove("hide") }, 7000);
+  setTimeout(function(){ second.classList.add("show") }, 7500);
+  setTimeout(function(){ second.classList.remove("show") }, 7500);
+  setTimeout(function(){ second.classList.add("hide") }, 9500);
+  setTimeout(function(){ form.classList.remove("hide") }, 12000);
+});
+function validateCode() {
+  let code = document.getElementById("code").value;
+  if (code !== "02112USABOSTON335148825") {
+    alert("Secret Code is incorrect");
+  } else {
+    window.open('file:///C:/BPMONLINE/SQLbuddies/css/congrats.html', "_self");
+  }
+  return false;
+}
